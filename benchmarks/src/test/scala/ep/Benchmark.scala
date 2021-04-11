5724da913f1dd332e96f8b9f2052c2a07f2167a9
George Heineman <heineman@cs.wpi.edu>
2021-04-10 22:32:17 -0400

    Incorporated trivially into benchmarks and generated results into README

--- benchmarks/src/test/scala/ep/Benchmark.scala
+++ benchmarks/src/test/scala/ep/Benchmark.scala
@@ -1,9 +1,5 @@
 package ep
 
-import ev.ep.i2.{EvalPower, Power}
-import ev.ep.m0.Lit
-import ev.ep.m1.Sub
-import ev.ep.m7i2.SimplifyPower
 import org.scalameter.KeyValue
 import org.scalameter.api._
 
@@ -31,6 +27,10 @@ object EvalBenchmark extends Bench.OfflineReport {
   }
   object evObjects {
     import ev.ep._
+    import ev.ep.i2.Power
+    import ev.ep.m0.Lit
+    import ev.ep.m1.Sub
+
     val startEval: Exp = new Power(new Lit(1.0), new Lit(1.0001))
     val evalExps = sizes.map(size => (0 until size).foldLeft(startEval)((current, _) => new Power(current, current)))
     val startSimplify: Exp = new Sub(new Lit(1.0), new Lit(1.0))
@@ -43,11 +43,24 @@ object interpreterObjects {
     val startSimplify: MergedExp = interpreter.ep.m7i2.MergedExpFactory.sub(interpreter.ep.m7i2.MergedExpFactory.lit(1.0), interpreter.ep.m7i2.MergedExpFactory.lit(1.0))
     val simplifyExps = sizes.map(size => (0 until size).foldLeft(startSimplify)((current, _) => interpreter.ep.m7i2.MergedExpFactory.power(current, current)))
   }
+  object triviallyObjects  {
+    import trivially.ep.m7i2.Exp
+    import trivially.ep.m7i2.finalized._
+    val startEval: Exp[Visitor] = new Power(new Lit(1.0), new Lit(1.0001))
+    val evalExps:Gen[trivially.ep.m7i2.Exp[trivially.ep.m7i2.finalized.Visitor]] = sizes.map(size => (0 until size).foldLeft(startEval)((current, _) => new Power(current, current)))
+    val startSimplify: Exp[Visitor] = new Sub(new Lit(1.0), new Lit(1.0))
+    val simplifyExps: Gen[Exp[Visitor]] = sizes.map(size => (0 until size).foldLeft(startSimplify)((current, _) => new Power(current, current)))
+  }
 
   performance of "eval" config(testConfig:_*) in {
     measure method "coco" in {
       using(cocoObjects.evalExps) in { exp =>
         cocoObjects.convert(exp).eval()
+      }
+    }
+    measure method "trivially" in {
+      using(cocoObjects.evalExps) in { exp =>
+        exp.getSelf.eval()
       }
     }
     measure method "oo" in {
@@ -57,7 +70,7 @@ object interpreterObjects {
     }
     measure method "ev" in {
       using(evObjects.evalExps) in { exp =>
-        exp.accept(new EvalPower)
+        exp.accept(new ev.ep.m7i2.EvalDivdMultNegTruncateEqualPower)
       }
     }
     measure method "interpreter" in {
@@ -73,6 +86,11 @@ object interpreterObjects {
         cocoObjects.convert(exp).simplify()
       }
     }
+    measure method "trivially" in {
+      using(cocoObjects.simplifyExps) in { exp =>
+        exp.getSelf.simplify()
+      }
+    }
     measure method "oo" in {
       using(ooObjects.simplifyExps) in { exp =>
         exp.simplify()
@@ -80,7 +98,7 @@ object interpreterObjects {
     }
     measure method "ev" in {
       using(evObjects.simplifyExps) in { exp =>
-        exp.accept(new SimplifyPower)
+        exp.accept(new ev.ep.m7i2.SimplifyPower)
       }
     }
     measure method "interpreter" in {
